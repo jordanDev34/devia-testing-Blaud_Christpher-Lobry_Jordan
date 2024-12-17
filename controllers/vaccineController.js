a2c47ca613660006863632059bee09c890964619
ChristopherBlaud <contact@cblaud.com>
2024-12-17 15:59:58 +0100

    update: add function to deleteVaccine

--- controllers/vaccineController.js
+++ controllers/vaccineController.js
@@ -12,3 +12,16 @@ exports.createVaccine = async (req, res) => {
       res.status(500).json({ error: error.message });
   }
 };
+
+exports.deleteVaccine = async (req, res) => {
+  try {
+      const vaccine = await Vaccine.findByPk(req.params.id);
+      if (!vaccine) return res.status(404).json({ error: 'Vaccine not found' });
+
+      await vaccine.destroy();
+      
+      res.status(204).send();
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
+};
--- routes/vaccineRoutes.js
+++ routes/vaccineRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const vaccineController = require('../controllers/vaccineController');
 
 router.post('/vaccines', vaccineController.createVaccine);
-router.delete('/vaccines/vaccine/:id', vaccineController.deletevaccine);
+router.delete('/vaccines/vaccine/:id', vaccineController.deleteVaccine);
 
 module.exports = router;

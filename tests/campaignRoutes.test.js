68b50006e310573672561067fa1e31a8edc8380d
Jordan LOBRY <jordan51rs@hotmail.fr>
2024-12-20 09:30:06 +0100

    add campaign test: get all campaigns

--- tests/campaignRoutes.test.js
+++ tests/campaignRoutes.test.js
@@ -33,6 +33,14 @@ describe('Campaign routes', () => {
     expect(res.body.location).to.equal('Paris');
   });
 
+  it('should return an array of campaigns', async () => {
+    const res = await request.get('/campaigns');
+
+    expect(res.status).to.equal(200);
+    expect(res.body).to.be.an('array');
+    expect(res.body.length).to.equal(1);
+  });
+
   it('should delete a campaign', async () => {
     const campaign = await request.post('/campaigns').send({
       name: 'Petit depistage',

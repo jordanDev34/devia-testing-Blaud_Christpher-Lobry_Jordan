c9f90b57d78c81a324d6584aef8823aca5600416
Jordan LOBRY <jordan51rs@hotmail.fr>
2024-12-20 09:54:50 +0100

    add campaign controller and route: get campaign by id

--- controllers/campaignController.js
+++ controllers/campaignController.js
@@ -29,6 +29,17 @@ exports.getAllCampaigns = async (req, res) => {
     }
 };
 
+exports.getCampaignById = async (req, res) => {
+    try {
+        const campaign = await Campaign.findByPk(req.params.id);
+        if (!campaign) return res.status(404).json({ error: 'Campaign not found' });
+
+        res.json(campaign);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
 exports.deleteCampaign = async (req, res) => {
     try {
         const campaign = await Campaign.findByPk(req.params.id);
--- routes/campaignRoutes.js
+++ routes/campaignRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const campaignController = require('../controllers/campaignController');
 
 router.get('/', campaignController.getAllCampaigns);
+router.get('/campaign/:id', campaignController.getCampaignById);
 router.post('/', campaignController.createCampaign);
 router.delete('/campaign/:id', campaignController.deleteCampaign);
 
